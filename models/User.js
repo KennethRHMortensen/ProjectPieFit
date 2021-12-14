9428f93990692c9b560167cab57381ba44623586
Kenneth <kenneth@kennt.dk>
2021-12-14 14:57:21 +0100

    fixed a lot

--- models/User.js
+++ models/User.js
@@ -1,5 +1,17 @@
 'use strict';
 
+module.exports = class Card {
+    constructor(workouttype, exercise, reps) {
+        this.workouttype = workouttype,
+        this.exercise = exercise,
+        this.reps = reps
+    }
+
+    toString() {
+        return `<tr><td>${this.workouttype}</td><td>${this.exercise}</td><td>${this.reps}</td></tr>`;
+    }
+};
+
 module.exports = class User {
     constructor(name, email, phone) {
         this.name = name,
@@ -11,3 +23,16 @@ module.exports = class User {
         return `<tr><td>${this.name}</td><td>${this.email}</td><td>${this.phone}</td></tr>`;
     }
 };
+// 'use strict';
+
+// module.exports = class User {
+//     constructor(name, email, phone) {
+//         this.name = name,
+//         this.email = email,
+//         this.phone = phone
+//     }
+
+//     toString() {
+//         return `<tr><td>${this.name}</td><td>${this.email}</td><td>${this.phone}</td></tr>`;
+//     }
+// };
--- models/handleSundry.js
+++ models/handleSundry.js
@@ -8,18 +8,52 @@ const dbp = require('./dbParams.js');
 const bcrypt = require('bcryptjs');
 
 module.exports = {
-    async updContacts(obj) {
+    async updUsers(obj) {
         const dbh = await maria.createConnection(dbp);
         let hashed = await bcrypt.hash(obj.POST.password, 10);
         let sql = `insert into user values('${obj.POST.name}', '${obj.POST.email}', '${obj.POST.phone}', '${hashed}')`;
         await dbh.query(sql);
     },
 
-    async showContacts () {
+    async updCards(obj) {
+        const dbh = await maria.createConnection(dbp);
+        let sql = `insert into card values('${obj.POST.workouttype}', '${obj.POST.exercise}', '${obj.POST.reps}')`;
+        await dbh.query(sql);
+    },
+    async updDrafts(obj) {
+        const dbh = await maria.createConnection(dbp);
+        let sql = `insert into draft values('${obj.POST.workouttype}', '${obj.POST.exercise}', '${obj.POST.reps}')`;
+        await dbh.query(sql);
+    },
+    async updCardsFromDrafts(obj) {
+        const dbh = await maria.createConnection(dbp);
+        let sql = `INSERT INTO card
+        SELECT * FROM draft;`;
+        function truncateTable() {
+            let sql = `TRUNCATE TABLE draft;`
+            dbh.query(sql);
+        }
+        await dbh.query(sql);
+        truncateTable();
+    },
+    
+    async showUsers () {
         const dbh = await maria.createConnection(dbp);
         const rows = await dbh.query('select * from user');
         return rows;
     },
+
+    async showCards () {
+        const dbh = await maria.createConnection(dbp);
+        const rows = await dbh.query('select * from card');
+        return rows;
+    },
+    async showDrafts () {
+        const dbh = await maria.createConnection(dbp);
+        const rows = await dbh.query('select * from draft');
+        return rows;
+    },
+
 
     async verify(obj) {
         const dbh = await maria.createConnection(dbp);
